5b0735b001c8d351f666e07f04cb68539e4ac303
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-03-29 00:44:39 +0100

    Add persons container

--- src/app/persons/persons.module.ts
+++ src/app/persons/persons.module.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { SharedModule } from '../shared';
 import { PersonsComponent } from './components/persons/persons.component';
+import { PersonsPageComponent } from './containers/persons-page/persons-page.component';
 import { PersonsRoutingModule } from './persons-routing.module';
 import { fromPerson } from './reducers';
 
 @NgModule({
-  declarations: [PersonsComponent],
+  declarations: [PersonsComponent, PersonsPageComponent],
   imports: [
     CommonModule,
     PersonsRoutingModule,
+    SharedModule,
     StoreModule.forFeature(fromPerson.personsFeatureKey, fromPerson.reducer),
   ],
 })
